b35d6927d7513e0101b6a6755ccbed4a79504813
Fazlul Kabir Rahat <110181350+fazlulkabirrahat@users.noreply.github.com>
2023-07-27 23:49:31 +0600

    Add files via upload
    assignment done

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  <script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          primary: '#F85559;',
+          titleColor:'#121212',
+          secondary:'#FFF',
+          descripColor:'#777',
+        }
+      }
+    }
+  }
+</script>
